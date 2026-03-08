9835afea1e62fd0e9f67b169101b06299085b39c
quetta1e30 <quetta1e30@gmail.com>
2026-03-08 18:54:55 +0800

    fix(slack): define use_thread variable before use in send()
    The use_thread variable was referenced but never defined, causing a NameError
    when sending any Slack message.
    
    Fixes #1713
    
    Signed-off-by: quetta1e30 <quetta1e30@gmail.com>

--- nanobot/channels/slack.py
+++ nanobot/channels/slack.py
@@ -82,6 +82,7 @@ class SlackChannel(BaseChannel):
             thread_ts = slack_meta.get("thread_ts")
             channel_type = slack_meta.get("channel_type")
             # Only reply in thread for channel/group messages; DMs don't use threads
+            use_thread = thread_ts is not None and channel_type != "im"
             thread_ts_param = thread_ts if use_thread else None
 
             # Slack rejects empty text payloads. Keep media-only messages media-only,
